0719536bdc099c232b2ccdaba8a248d15a319292
Alessandro Tulioli <alessandro.smarapd@hotmail.com>
2021-10-21 22:34:28 -0300

    Criado Controller Fonecedor e Automapper

---
+++ src/DevIO.Api/Controllers/FornecedoresController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using DevIO.Api.Dto;
+using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/controller")]
+    public class FornecedoresController : MainController
+    {
+        private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IMapper _mapper;
+
+        public FornecedoresController(IFornecedorRepository fornecedorRepository, IMapper mapper)
+        {
+            _fornecedorRepository = fornecedorRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<FornecedorDto>> ObterTodos()
+        {
+            var fornecedor = _mapper.Map<IEnumerable<FornecedorDto>>(await _fornecedorRepository.ObterTodos());
+
+            return fornecedor;
+        }
+    }
+}
